c92e4ab471c1bd922a70a8f001104a4811ca9b10
Ajay Shukla <ajayshukla.bhu@gmail.com>
2019-12-08 13:28:44 +0530

    hello user basic structure

---
+++ src/test/java/com/todo/hellouser/HelloUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todo.hellouser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
